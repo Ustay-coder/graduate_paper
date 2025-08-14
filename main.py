e66bb22bbe161ec9aab94df5f843abdfda5eb491
김지안 <gimjian@gimjian-ui-Macmini.local>
2025-08-14 19:28:44 +0900

    <type>: feat <title> main function의 backbone 코드 구성

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-
+from generator.comparison.generator import ComparisonGenerator
+from generator.control.generator import ControlGenerator
+from evaluator.evaluator import Evaluator
 
 def main(): 
 
-    # 1. 
+    # 1. 질문지를 로드함. 
+    test_questions_list = ['고령자들을 위한 최적의 암 발견 방법은 무엇일까?']
+    # 2. 동일한 질문을 가지고 generator/comparsion, generator/control 두 그룹에서 데이터를 생성함. 
+    for question in test_questions_list:
+        # 2-1. 질문을 가지고 generator/comparsion 그룹에서 데이터를 생성함.
+        comparison_generator = ComparisonGenerator(llm, prompt)
+        data_comparison = comparison_generator.generate(question)
+        # 2-2. 질문을 가지고 generator/control 그룹에서 데이터를 생성함. 
+        control_generator = ControlGenerator(llm, prompt)
+        data_control = control_generator.generate(question)
 
+    # 3. 생성된 데이터를 evaluator에서 평가함. 
+    evaluator = Evaluator(llm, prompt)
+    data_comparison_evaluated = evaluator.evaluate(data_comparison)
+    data_control_evaluated = evaluator.evaluate(data_control)
+    
+    # 4. 평가된 데이터를 비교함.
+    analyze = evaluator.analyze(data_comparison_evaluated, data_control_evaluated)
+
+    # 5. 비교 결과를 출력/저장함. 
+    print(analyze)
+    evaluator.save_result(analyze)
 
 if __name__ == "__main__":
-    pass
+    main()
